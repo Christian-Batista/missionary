8a81e84947b85bd097ad019acc2e5d51bf7cf0b2
ChristianBatista <elcatchon@gmail.com>
2024-08-23 22:06:54 -0400

    In this commit we add a button to log in and register for mobiles and we finish some styling for the registration page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,54 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+        './vendor/livewire/**/*.blade.php',
     ],
 
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            fontSize: {
+                xs: '0.2rem',
+                sm: '0.8rem',
+                base: '1rem',
+                xl: '1.25rem',
+                '2xl': '1.563rem',
+                '3xl': '1.953rem',
+                '4xl': '2.441rem',
+                '5xl': '3.052rem',
+            },
+            fontWeight: {
+                thin: '50',
+                hairline: '100',
+                extralight: '200',
+                light: '300',
+                normal: '400',
+                medium: '500',
+                semibold: '600',
+                bold: '700',
+                extrabold: '800',
+                'extra-bold': '800',
+                black: '900',
+            },
+            boxShadow: {
+                '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+            },
+            container: {
+                center: true,
+            },
+            colors: {
+                'azul-boton': '#357CA1',
+                'gris-obscuro': '#444444',
+                gray: {
+                    400: '#9CA3AF',
+                    // Add gray.500 if needed
+                    500: '#6B7280',
+                },
             },
         },
         screens: {
@@ -22,39 +64,8 @@ export default {
             'lg': '1160px',   // Large devices (laptops)
             'xl': '1280px',   // Extra large devices (computadoras de escritorio)
             '2xl': '1536px',  // 2x Extra large (grandes pantallas)
-          },
-          colors: {
-            'azul-boton': '#357CA1',
-            'gris-obscuro': '#444444'
-          },
-          boxShadow: {
-            '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
-          },
-          fontSize: {
-            xs: '0.2rem',
-            sm: '0.8rem',
-            base: '1rem',
-            xl: '1.25rem',
-            '2xl': '1.563rem',
-            '3xl': '1.953rem',
-            '4xl': '2.441rem',
-            '5xl': '3.052rem',
-          },
-          fontWeight: {
-            thin: '50',
-            hairline: '100',
-            extralight: '200',
-            light: '300',
-            normal: '400',
-            medium: '500',
-            semibold: '600',
-            bold: '700',
-            extrabold: '800',
-            'extra-bold': '800',
-            black: '900',
-          }
-          
+        },
     },
 
-    plugins: [forms],
+    plugins: [forms, require('tailwind-hamburgers')], 
 };
